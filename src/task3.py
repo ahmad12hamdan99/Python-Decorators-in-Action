749a509165a94c32c06ebb5719092d02fa308ff7
ahmad12hamdan99 <ahmad12hamdan99@gmail.com>
2022-09-13 11:45:25 +0300

    Task3 without log file

---
+++ src/task3.py
@@ -0,0 +1,19 @@
+from time import time
+
+class decorator_1:
+
+    def __init__(self, func):
+        self.function = func
+        self.count = 0
+        self.ltime = 0.0
+    def __call__(self, *args, **kwargs):
+        start_time = time()
+        result = self.function(*args, **kwargs)
+        end_time = time()
+        self.ltime = end_time-start_time
+        self.count += 1
+        # print("Execution took {} seconds".format(self.ltime))
+        print('{} call {} excuted in {} sec'.format(self.function.__name__ , self.count, self.ltime))
+        return result
+
+
