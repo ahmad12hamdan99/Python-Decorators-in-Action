9bd3ea86f2971293b084905dbfb846168b12f345
ahmad12hamdan99 <ahmad12hamdan99@gmail.com>
2022-09-17 16:38:59 +0300

    task3 __inspector

--- src/main_1.py
+++ src/main_1.py
@@ -1,7 +1,7 @@
 import random
-from task3 import decorator_1
+from task3 import decorator_3
 
-@decorator_1
+@decorator_3
 def func():
     print("I am ready to Start")
     result = 0
@@ -9,7 +9,7 @@ def func():
     for i in range(n):
         result += (i**2)
         
-@decorator_1
+@decorator_3
 def funx(n=2, m=5):
     print("I am ready to do serious stuff")
     max_val = float('-inf')
@@ -19,7 +19,7 @@ def funx(n=2, m=5):
         if i > max_val: 
             max_val = i
     
-if __name__ == "__main_1__": 
+if __name__ == "__main__": 
     func()
     funx()
     func()
--- src/task3.py
+++ src/task3.py
@@ -1,11 +1,15 @@
 from time import time
 
-class decorator_1:
+class decorator_3:
 
     def __init__(self, func):
         self.function = func
         self.count = 0
         self.ltime = 0.0
+
+    def __inspector(self,*args, **kwargs):
+        discription = f'Name:\t{self.function.__name__}\n'
+
     def __call__(self, *args, **kwargs):
         start_time = time()
         result = self.function(*args, **kwargs)
