ad2f35828ae9f0ea92d157e3ad6d8e52a621be22
ahmad12hamdan99 <ahmad12hamdan99@gmail.com>
2022-09-20 09:14:29 +0300

    final commit

--- src/main.py
+++ src/main.py
@@ -1,8 +1,29 @@
 import math
+import random
 from task1 import decorator_1
 from task2 import decorator_2
 from task3 import decorator_3
 from task4 import decorator_4
+
+
+@decorator_1
+def func():
+    print("I am ready to Start")
+    result = 0
+    n =  random.randint(10,751)
+    for i in range(n):
+        result += (i**2)
+        
+@decorator_1
+def funx(n=2, m=5):
+    print("I am ready to do serious stuff")
+    max_val = float('-inf')
+    n =  random.randint(10,751)
+    res = [pow(i,2) for i in range(n)]
+    for i in res:
+        if i > max_val: 
+            max_val = i
+
 
 
 @decorator_4
@@ -27,7 +48,7 @@ def solver(a, b, c):
     elif dis == 0:
         return [-b / (2 * a)]
     else:
-        #print("Complex Roots")
+        #Complex Roots
         return [str(-b / (2 * a)) + " + i" + str(sqrt_val) , str(-b / (2 * a)) + " - i" + str(sqrt_val)]
 
 
@@ -51,6 +72,12 @@ def err(n):
     return 1/n
 
 if __name__ == '__main__':
+
+    func()
+    funx()
+    func()
+    funx()
+    func()
 
     solver(1,2,3)
     pascal(5)
