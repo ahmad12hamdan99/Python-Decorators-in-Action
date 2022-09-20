7373d4669009d6bfe199cb5e9eab210426f0d38f
ahmad12hamdan99 <ahmad12hamdan99@gmail.com>
2022-09-20 08:16:32 +0300

    fixing output

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ from task3 import decorator_3
 from task4 import decorator_4
 
 
-@decorator_4
-@decorator_3
+#@decorator_4
+@decorator_2
 def solver(a, b, c):
     """
     This function does something useful 
@@ -31,19 +31,19 @@ def solver(a, b, c):
     else:
         #print("Complex Roots")
         return [str(-b / (2 * a)) + " + i" + str(sqrt_val) , str(-b / (2 * a)) + " - i" + str(sqrt_val)]
-@decorator_4
-@decorator_3
+#@decorator_4
+@decorator_2
 def pascal(n):
     """
-    This function prints the pascal triangle
-    :param n: number of rows
-    """ 
-    for i in range(n):
-        # adjust space
-        print(' '*(n-i), end='')
-
-        # compute power of 11
-        print(' '.join(map(str, str(11**i))))
+    This function print pascal triangle
+    :param n: The number of pascal triangle rows
+    This function return None
+    """
+    main_v = [1]
+    added_v = [0]
+    for _ in range(n):
+        print(main_v)
+        main_v = [left + right for left, right in zip(main_v + added_v, added_v + main_v)]
 
 @decorator_4
 @decorator_3
@@ -51,18 +51,11 @@ def err(n):
     return 1/n
 
 if __name__ == '__main__':
-    
-    # pascal_triangle(5)
-    # quadratic_equation_solver(1,2,3)
-    # pascal_triangle(5)
-    # quadratic_equation_solver(1,2,3)
-    # quadratic_equation_solver(1,2,3)
-    # pascal_triangle(10)
 
     solver(1,2,3)
     pascal(5)
     solver(0,0,0)
     pascal(0)
-    decorator_3.ranking()
-    err(0)
+    #decorator_3.ranking()
+    #err(0)
 
